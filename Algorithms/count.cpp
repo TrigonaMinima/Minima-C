42fecc5c07371a9bae88bb9763a36912a63dc423
TrigonaMinima <shivamrana95@yahoo.in>
2015-05-19 02:43:51 +0530

    Commented

--- Algorithms/count.cpp
+++ Algorithms/count.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 void count_sort(int *arr, int size, int start, int end)
 {
+    // Creates a bucket of the size (end - start + 1) to hold the count
+    // of the occurrence of the array elements in the range. King of like
+    // the frequency.
     int *bucket = new int[end - start + 1];
     for(int i = 0; i < (end - start + 1); i++)
         bucket[i] = 0;
@@ -15,6 +18,7 @@ void count_sort(int *arr, int size, int start, int end)
     for (int i = 0; i < size; ++i)
         bucket[arr[i] - start] += 1;
 
+    // prints the sorted array.
     for (int i = 0; i < (end - start + 1); ++i)
     {
         for(int j = 0; j < bucket[i]; j++)
