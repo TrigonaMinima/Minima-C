e29d12631cfa84e600c83cd7d06c34a78fbc89e0
TrigonaMinima <shivamrana95@yahoo.in>
2015-05-23 23:19:12 +0530

    Directory class
    A new header file

---
+++ OOPs/12th Proj/Linux Version/directory.h
@@ -0,0 +1,22 @@
+# include <iostream>
+# include <stdlib.h>
+# include <string.h>
+
+using namespace std;
+
+class directory
+{
+    int record_id;
+    record r;
+public:
+    void menu();
+    void add_record();
+    // void delete_record();
+    // void modifify_record();
+    // char check(string);
+    // void view_record();
+    // void search_record();
+    // void init();
+    // void view1();
+    // int  no_of_records();
+};
