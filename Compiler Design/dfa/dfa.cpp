216edeee91f1d4b275a8ab26fb68dea7653ed769
TrigonaMinima <shivamrana95@yahoo.in>
2015-09-08 00:00:17 +0530

    DFA

--- Compiler Design/dfa/dfa.cpp
+++ Compiler Design/dfa/dfa.cpp
@@ -1,4 +1,4 @@
-// Mealy Machine
+// Deterministic Finite Automata
 
 # include <iostream>
 # include <fstream>
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-// Creates the mealy type of data structure.
+// Creates the dfa type of data structure.
 // start symbol, no. of states, no. of inputs, table.
-struct mealy
+struct dfa
 {
     string start;
-    vector<string> states, inputs;
-    map< string, map<string, map<string, string> > > table;
+    vector<string> states, inputs, final;
+    map< string, map<string, string> > table;
 };
 
 // Prints the vector.
@@ -31,9 +31,9 @@ void print_vector(vector<string> v)
 }
 
 // Genrates the transition table kind of dictionary.
-mealy gen_table(vector<string> v)
+dfa gen_table(vector<string> v)
 {
-    mealy inp;
+    dfa inp;
     int size = v.size(), isize;
 
     // Start symbol
@@ -47,86 +47,99 @@ mealy gen_table(vector<string> v)
         inp.states.push_back(temp);
     // print_vector(inp.states);
 
-    // All inputs extracted.
+    // All final states extracted.
     stringstream ss2(v[2]);
     while (ss2 >> temp)
+        inp.final.push_back(temp);
+    // print_vector(inp.inputs);
+
+    // All inputs extracted.
+    stringstream ss3(v[3]);
+    while (ss3 >> temp)
         inp.inputs.push_back(temp);
     isize = inp.inputs.size();
     // print_vector(inp.inputs);
 
     // Creates the table.
-    for(int i=3; i<size; i++)
+    for(int i=4; i<size; i++)
     {
         if(v[i] == "")
             break;
-        map<string, map<string, string> > row;
+        map<string, string> row;
         vector<string> rows;
         stringstream ss(v[i]);
         while (ss >> temp)
             rows.push_back(temp);
 
-        // print_vector(rows);
+        print_vector(rows);
 
         for(int j=0; j<isize; j++)
-        {
-            map<string, string> element;
-
-            element["out"] = rows[j*2];
-            element["state"] = rows[j*2+1];
-
-            row[inp.inputs[j]] = element;
-        }
-        inp.table[inp.states[i-3]] = row;
+            row[inp.inputs[j]] = rows[j];
+        inp.table[inp.states[i-4]] = row;
     }
     return inp;
 }
 
 // Checks the validity of the user input
-void check_validity(mealy inp, string input)
+int check_validity(dfa inp, string input)
 {
-    int size = input.size(), i=0;
+    int size = input.size(), i = 0, flag = 1, size1 = 0;
     string current = inp.start;
     string output;
     bool exists;
-    cout << "\n --------------- ------- -------- ------------ " << endl;
-    cout << "| Current State | Input | Output | Next State |" << endl;
-    cout << " --------------- ------- -------- ------------ " << endl;
+    cout << "\n --------------- ------- ------------ " << endl;
+    cout << "| Current State | Input | Next State |" << endl;
+    cout << " --------------- ------- ------------ " << endl;
 
     do
     {
         exists = inp.table[current].find(string(1, input[i])) == inp.table[current].end();
         if(!exists)
         {
-            cout << "|\t" << current << "\t|   "<< input[i] << "   |   ";
-            output = inp.table[current][string(1, input[i])]["out"];
+            cout << "|\t" << current << "\t|   "<< input[i] << "   |     ";
+            output = inp.table[current][string(1, input[i])];
             if (output == "-1")
             {
                 cout << "\nInput symbol \"" << input[i] << "\" at state "
                 << current << " is not recognized!!";
+                flag = 0;
                 break;
             }
-            current = inp.table[current][string(1, input[i])]["state"];
-            cout << output << "    |     " << current << "     |\n";
+            current = output;
+            cout << current << "     |\n";
             i++;
             if(i == size)
-                cout << " --------------- ------- -------- ------------ " << endl;
+                cout << " --------------- ------- ------------ " << endl;
+
+            size1 = inp.final.size();
+            for (int k = 0; k < size1; ++k)
+            {
+                if (current == inp.final[k])
+                    flag = 1;
+                else
+                    flag = 0;
+            }
         }
         else
         {
-            cout << " --------------- ------- -------- ------------ " << endl;
+            cout << " --------------- ------- ------------ " << endl;
             cout << "\nInput symbol \"" << input[i] << "\" at state \""
             << current << "\" is not recognized!!" << endl;
+            flag = 0;
             break;
         }
     }
     while(i != size);
+
+
+    return flag;
 }
 
 int main()
 {
     vector<string> data;    // stores file data
     string s;
-    mealy inp;
+    dfa inp;
 
     // read input file
     ifstream f("input.txt");
@@ -145,7 +158,10 @@ int main()
     getline(cin, s);
 
     // Check the validity of the string & display the output.
-    check_validity(inp, s);
+    if (check_validity(inp, s))
+        cout << "\nString accepted!!\n";
+    else
+        cout << "\nString not accepted!!\n";
 
     return 0;
 }
--- Compiler Design/readme.md
+++ Compiler Design/readme.md
@@ -3,10 +3,37 @@ Compiler Design Lab
 
 1. lex.txt
 2. Regex Parser
-3. DFA
+3. [DFA](#dfa)
 4. [Mealy Machine](#mealy)
 5. [Moore Machine](#moore)
 6. NFA to DFA
+
+
+## <a name="dfa"></a> Deterministic Finite Automaton
+
+Input Structure:
+
+```
+initial state
+distinct states
+distinct inputs
+output(i) for each input on state
+```
+
+The dictionary which is created in memory.
+
+```
+{
+    "q1": {
+        "0": <state>,
+        "1": <state>
+    },
+    "q2": {
+        "0": <state>,
+        "1": <state>
+    }
+}
+```
 
 
 ## <a name="mealy"></a> Mealy Machine
@@ -47,7 +74,7 @@ The dictionary which is created in memory.
 }
 ```
 
-## <a name="mealy"></a> Mealy Machine
+## <a name="moore"></a> Moore Machine
 
 Input Structure:
 
